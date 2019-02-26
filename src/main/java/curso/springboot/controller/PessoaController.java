add1a434a6487995d4713969e04076d86b4024ca
guilherme-santana <santana.guilherme93@gmail.com>
2019-02-26 00:09:05 -0300

    Pesquisar Pessoas

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
@@ -81,6 +83,14 @@ public class PessoaController {
 		//adiciona na viw a lista de pessoas. Atributo pessoas criado no html
 		andView.addObject("pessoas", PessoasIt);
 		//passa a lista de objetos vazia para poder carregar a tela de cadastro sem erros
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
+	}
+	
+	@PostMapping("**/pesquisapessoa")
+	public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa){
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		andView.addObject("pessoas", pessoaRepository.findPessoaByName(nomepesquisa.toLowerCase()));
 		andView.addObject("pessoaobj", new Pessoa());
 		return andView;
 	}
